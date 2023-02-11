324c928ae29e07296ba51594532fc0c787a00110
Hemanta Gayen <69314427+hemucode@users.noreply.github.com>
2023-02-11 18:54:02 +0530

    fg

--- Chrome/background.js
+++ Chrome/background.js
@@ -3,4 +3,15 @@ importScripts("lib/chrome.js");
 importScripts("lib/runtime.js");
 importScripts("lib/common.js");
 
+var MOBILE_CHROME_USER_AGENT = 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1';
+chrome.webRequest.onBeforeSendHeaders.addListener(
+    function(details) {
+        for (var i = 0; i < details.requestHeaders.length; ++i) {
+            if (details.requestHeaders[i].name === 'User-Agent') {
+                details.requestHeaders[i].value = MOBILE_CHROME_USER_AGENT;
+                break;
+            }
+        }
+        return {requestHeaders: details.requestHeaders};
+}, {urls: ['<all_urls>']}, ['blocking', 'requestHeaders']);
 
