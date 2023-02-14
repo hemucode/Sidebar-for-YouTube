321a0666b3c86ccd7683f8e856aa6b4a4c03ddac
Hemanta Gayen <69314427+hemucode@users.noreply.github.com>
2023-02-14 13:01:46 +0530

    bn

--- popup for youtube/data/settings.js
+++ popup for youtube/data/settings.js
@@ -1,6 +1,4 @@
 window.addEventListener("load", function () {
-  var iframe = document.getElementById("sidebar-iframe");
-  if (iframe.src === "about:blank") iframe.src = "https://www.youtube.com";
   /*  */
   var head = document.documentElement || document.head || document.querySelector("head");
   if (head) {
@@ -10,7 +8,7 @@ window.addEventListener("load", function () {
       link.type = "text/css";
       link.rel = "stylesheet";
       link.setAttribute("id", "youtube-sidebar-style-css");
-      link.href = chrome.runtime.getURL("data/sidebar/inject/inject.css");
+      link.href = chrome.runtime.getURL("data/button.css");
       head.appendChild(link);
     }
     /*  */
@@ -18,12 +16,12 @@ window.addEventListener("load", function () {
     table.setAttribute("id", "top-button-table");
     /*  */
     var tr = document.createElement("tr");
-    var faqicon = chrome.runtime.getURL("data/sidebar/icons/faq.png");
-    var backicon = chrome.runtime.getURL("data/sidebar/icons/back.png");
-    var homeicon = chrome.runtime.getURL("data/sidebar/icons/home.png");
-    var popouticon = chrome.runtime.getURL("data/sidebar/icons/popout.png");
-    var forwardicon = chrome.runtime.getURL("data/sidebar/icons/forward.png");
-    var settingsicon = chrome.runtime.getURL("data/sidebar/icons/settings.png");
+    var faqicon = chrome.runtime.getURL("data/icons/faq.png");
+    var backicon = chrome.runtime.getURL("data/icons/back.png");
+    var homeicon = chrome.runtime.getURL("data/icons/home.png");
+    var popouticon = chrome.runtime.getURL("data/icons/popout.png");
+    var forwardicon = chrome.runtime.getURL("data/icons/forward.png");
+    var settingsicon = chrome.runtime.getURL("data/icons/settings.png");
     /*  */
     var backButton = document.createElement("td");
     backButton.setAttribute("title", "Back");
@@ -32,44 +30,38 @@ window.addEventListener("load", function () {
     backButton.addEventListener("click", function () {window.history.back()});
     tr.appendChild(backButton);
     /*  */
+
     var settingsButton = document.createElement("td");
     settingsButton.setAttribute("title", "Settings");
     settingsButton.setAttribute("id", "settings-button");
     settingsButton.style.backgroundImage = "url(" + settingsicon + ')';
-    settingsButton.addEventListener("click", function () {background.send("settings")});
+    settingsButton.addEventListener("click", function () {
+      window.history.back()
+    });
     tr.appendChild(settingsButton);
     /*  */
+
     var homeButton = document.createElement("td");
     homeButton.setAttribute("title", "Home");
     homeButton.setAttribute("id", "home-button");
     homeButton.style.backgroundImage = "url(" + homeicon + ')';
-    homeButton.addEventListener("click", function () {document.location.reload()});
+    homeButton.addEventListener("click", function () {window.history.back()});
     tr.appendChild(homeButton);
     /*  */
-    var popoutButton = document.createElement("td");
-    popoutButton.setAttribute("title", "Pop-out");
-    popoutButton.setAttribute("id", "pop-out-button");
-    popoutButton.style.backgroundImage = "url(" + popouticon + ')';
-    popoutButton.addEventListener("click", function () {background.send("pop-out")});
-    tr.appendChild(popoutButton);
+
     /*  */
     var faqButton = document.createElement("td");
     faqButton.setAttribute("id", "faq-button");
     faqButton.setAttribute("title", "Support page");
     faqButton.style.backgroundImage = "url(" + faqicon + ')';
-    faqButton.addEventListener("click", function () {background.send("faq")});
+    faqButton.addEventListener("click", function () {
+      url = "https://www.downloadhub.cloud/2023/02/sidebar.html#CSS1"
+      window.open(url , '_blank');
+    });
     tr.appendChild(faqButton);
-    /*  */
-    var forwardButton = document.createElement("td");
-    forwardButton.setAttribute("title", "Forward");
-    forwardButton.setAttribute("id", "forward-button");
-    forwardButton.style.backgroundImage = "url(" + forwardicon + ')';
-    forwardButton.addEventListener("click", function () {window.history.forward()});
-    tr.appendChild(forwardButton);
-    /*  */
+
     table.appendChild(tr);
     document.body.appendChild(table);
   }
 });
-
 
